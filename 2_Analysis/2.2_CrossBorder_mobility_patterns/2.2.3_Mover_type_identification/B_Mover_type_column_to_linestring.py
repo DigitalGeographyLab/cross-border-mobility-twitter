8583cd2fa020b5cc28a04969874304d286923a06
Samuli Massinen <smassinen@hotmail.com>
2019-09-03 15:05:14 +0300

    Update scripts

---
+++ 2_Analysis/2.2_CrossBorder_mobility_patterns/2.2.3_Mover_type_identification/B_Mover_type_column_to_linestring.py
@@ -0,0 +1,30 @@
+"""
+This script adds identified mover type labels to Twitter LineString dataset.
+
+@author: smassine
+"""
+
+import pandas as pd
+import geopandas as gpd
+import pickle
+
+# Open LineString dataset
+with open(r'C:\LocalData\smassine\Gradu\Data\Twitter\Luxemburg_border_region\API\Geotagged\LineString\Greater_Region_crs_update_missing_countries.pkl', 'rb') as f:
+    data = pickle.load(f)
+    
+text_file = open(r"C:\LocalData\smassine\Gradu\Data\Twitter\Luxemburg_border_region\API\Geotagged\Commuters\NullTripsExclude\commuter_userids_20_threshold.txt", "r")
+id_list = text_file.read().split('\n')
+
+commuters = data.loc[data['userid'].isin(id_list)]
+others = data.loc[~data['userid'].isin(id_list)]
+
+commuters['commuterType'] = 'CB-commuter'
+others['commuterType'] = 'Infrequent border crosser'
+
+merged_df = pd.concat([commuters, others], ignore_index = True)
+
+merged_df.to_pickle(r"C:\LocalData\smassine\Gradu\Data\Twitter\Luxemburg_border_region\API\Geotagged\LineString\CommuterColumn\Greater_Region_mover_type.pkl")
+
+merged_df = merged_df.astype({"origTime": str, "destTime": str, 'avgTime': str, 'duration': str})
+merged_df = gpd.GeoDataFrame(merged_df, geometry='geometry')
+merged_df.to_file(r"C:\LocalData\smassine\Gradu\Data\Twitter\Luxemburg_border_region\API\Geotagged\LineString\CommuterColumn\SHP\Greater_Region_LineString_mover_type.shp")
